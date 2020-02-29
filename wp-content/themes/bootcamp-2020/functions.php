4e6dd16a35208e2cf53a468854f5284795439513
abhishek-pokhriyal <pokhi@coloredcow.com>
2020-02-29 17:45:17 +0530

    Day 4

--- wp-content/themes/bootcamp-2020/functions.php
+++ wp-content/themes/bootcamp-2020/functions.php
@@ -1,136 +1,31 @@
 <?php
-/**
- * BootCamp 2020 functions and definitions
- *
- *
- * @package BootCamp_2020
- * @since 1.0.0
- */
 
+function mytheme_setup() {
+ 	/*
+ 	 * Enable support for Post Thumbnails on posts and pages.
+ 	 */
+ 	add_theme_support( 'post-thumbnails' );
 
-if ( ! function_exists( 'bootcamp2020_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function bootcamp2020_setup() {
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+ 	// This theme uses wp_nav_menu() in two locations.
+ 	register_nav_menus(
+ 		array(
+ 			'menu-1' => 'Primary',
+ 			'footer' => 'Footer Menu',
+ 			'social' => 'Social Links Menu',
+ 		)
+ 	);
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-		set_post_thumbnail_size( 1568, 9999 );
-
-		// This theme uses wp_nav_menu() in two locations.
-		register_nav_menus(
-			array(
-				'menu-1' => __( 'Primary', 'bootcamp-2020' ),
-				'footer' => __( 'Footer Menu', 'bootcamp-2020' ),
-				'social' => __( 'Social Links Menu', 'bootcamp-2020' ),
-			)
-		);
-
-		/**
-		 * Add support for core custom logo.
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 190,
-				'width'       => 190,
-				'flex-width'  => false,
-				'flex-height' => false,
-			)
-		);
-
-
-		// Add support for Block Styles.
-		add_theme_support( 'wp-block-styles' );
-
-		// Add support for full and wide align images.
-		add_theme_support( 'align-wide' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style-editor.css' );
-
-		// Add custom editor font sizes.
-		add_theme_support(
-			'editor-font-sizes',
-			array(
-				array(
-					'name'      => __( 'Small', 'bootcamp-2020' ),
-					'shortName' => __( 'S', 'bootcamp-2020' ),
-					'size'      => 19.5,
-					'slug'      => 'small',
-				),
-				array(
-					'name'      => __( 'Normal', 'bootcamp-2020' ),
-					'shortName' => __( 'M', 'bootcamp-2020' ),
-					'size'      => 22,
-					'slug'      => 'normal',
-				),
-				array(
-					'name'      => __( 'Large', 'bootcamp-2020' ),
-					'shortName' => __( 'L', 'bootcamp-2020' ),
-					'size'      => 36.5,
-					'slug'      => 'large',
-				),
-				array(
-					'name'      => __( 'Huge', 'bootcamp-2020' ),
-					'shortName' => __( 'XL', 'bootcamp-2020' ),
-					'size'      => 49.5,
-					'slug'      => 'huge',
-				),
-			)
-		);
-
-		// Editor color palette.
-		add_theme_support(
-			'editor-color-palette',
-			array(
-				array(
-					'name'  => __( 'Dark Gray', 'bootcamp-2020' ),
-					'slug'  => 'dark-gray',
-					'color' => '#111',
-				),
-				array(
-					'name'  => __( 'Light Gray', 'bootcamp-2020' ),
-					'slug'  => 'light-gray',
-					'color' => '#767676',
-				),
-				array(
-					'name'  => __( 'White', 'bootcamp-2020' ),
-					'slug'  => 'white',
-					'color' => '#FFF',
-				),
-			)
-		);
-
-		// Add support for responsive embedded content.
-		add_theme_support( 'responsive-embeds' );
-	}
-endif;
-add_action( 'after_setup_theme', 'bootcamp2020_setup' );
-
-/**
- * Enqueue scripts and styles.
- */
-function twentynineteen_scripts() {
-	wp_enqueue_style( 'twentynineteen-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+ 	/**
+ 	 * Add support for core custom logo.
+ 	 */
+ 	add_theme_support(
+ 		'custom-logo',
+ 		array(
+ 			'height'      => 190,
+ 			'width'       => 190,
+ 			'flex-width'  => false,
+ 			'flex-height' => false,
+ 		)
+ 	);
 }
-add_action( 'wp_enqueue_scripts', 'twentynineteen_scripts' );
+add_action( 'after_setup_theme', 'mytheme_setup' );
--- wp-content/themes/bootcamp-2020/index.php
+++ wp-content/themes/bootcamp-2020/index.php
@@ -1,4 +1,2 @@
 <?php
-
-
-echo 'Bootcamp index theme';
+// Nothing here!
